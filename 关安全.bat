270898e96c1384eddc3b86d5b0ca81befcb8f1d0
亠口冗 <91411081+ll2310782478@users.noreply.github.com>
2024-10-24 17:10:48 +0800

    Update 关安全.bat

--- 关安全.bat
+++ 关安全.bat
@@ -1,3 +1,11 @@
 @echo off
-%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
-cd /d "%~dp0"
+setlocal
+
+rem 禁用实时保护
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableRealtimeMonitoring /t REG_DWORD /d 1 /f
+
+rem 重启 Windows Defender 服务
+net stop WinDefend
+net start WinDefend
+
+endlocal
